3072a34382e3cee68cd8e536f8c3c698b076803e
reiss-d <reiss-d@outlook.com>
2023-09-11 08:54:38 +0100

    perf(uft): add pure comments

--- packages/uft/src/internal/builtin.ts
+++ packages/uft/src/internal/builtin.ts
@@ -3,10 +3,12 @@
 export const proto = Object.prototype
 export const fnProto = Function.prototype
 
-const uncurryThis = fnProto.bind.bind(fnProto.call, fnProto.call)
+// TODO(fix): investigate swc stripping out the pure comments
+
+const uncurryThis = /*#__PURE__*/ fnProto.bind.bind(fnProto.call, fnProto.call)
 const callable = <T>(fn: Function): T => uncurryThis(fn.call) as T
 
-export const hasOwn = callable<
+export const hasOwn = /*#__PURE__*/ callable<
    (obj: object, v: PropertyKey) => boolean
 >(proto.hasOwnProperty)
 
