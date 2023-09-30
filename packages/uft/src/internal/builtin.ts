d820c7c9ddd69771fe1a679290ea44820d8d7543
reiss-d <reiss-d@outlook.com>
2023-09-30 01:02:14 +0100

    fix: improper binding of `hasOwn`

--- packages/uft/src/internal/builtin.ts
+++ packages/uft/src/internal/builtin.ts
@@ -6,7 +6,7 @@ export const fnProto = Function.prototype
 // TODO(fix): investigate swc stripping out the pure comments
 
 const uncurryThis = /*#__PURE__*/ fnProto.bind.bind(fnProto.call, fnProto.call)
-const callable = <T>(fn: Function): T => uncurryThis(fn.call) as T
+const callable = <T>(fn: Function): T => uncurryThis(fn) as T
 
 export const hasOwn = /*#__PURE__*/ callable<
    (obj: object, v: PropertyKey) => boolean
